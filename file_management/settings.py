83dee7545fd8f8ed8ba7277af2119abe341bd35a
Frirsta <frista-8@hotmail.com>
2023-05-12 11:19:10 +0200

    Fix css

--- file_management/settings.py
+++ file_management/settings.py
@@ -130,6 +130,9 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR, 'static']
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
